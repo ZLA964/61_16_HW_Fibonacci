dacbbb0ee376c4caa494cc291654cdf27dae827e
לב לאונטייב <152332597+ZLA964@users.noreply.github.com>
2024-11-25 14:08:32 +0300

    first var

---
+++ src/telran/fibonacci/FibonacciAppl.java
@@ -0,0 +1,19 @@
+package telran.fibonacci;
+
+import telran.fibonacci.model.Fibonacci;
+
+public class FibonacciAppl {
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci(8);
+        // TODO Homework
+        // print all numbers, and reduce sum
+        // 1,1,2,3,5,8,13,21
+        // sum = 54
+        int sum = 0;
+        for(Integer numeric : fibonacci){
+            System.out.print(numeric+", ");
+            sum += numeric;
+        }
+        System.out.println("\nSum= " + sum);
+    }
+}
---
+++ src/telran/fibonacci/model/FibonacciIterator.java
@@ -0,0 +1,41 @@
+package telran.fibonacci.model;
+
+import java.util.Iterator;
+
+public class FibonacciIterator implements Iterator<Integer> {
+    private int full;
+    private int start = 1;
+    int i;
+    private int current;
+    private int previous;
+
+    public FibonacciIterator(int quantity   ) {
+        this.full = quantity;
+        this.previous= start;
+        int i = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return i < full ;
+    }
+
+    @Override
+    public Integer next() {
+       if(i == 0 )  {
+           previous = start;
+           i++;
+           return start;
+       }
+       if( i == 1 ) {
+           i++;
+           current = previous;
+       }  else {
+           i++;
+           int summ = previous + current;
+           previous = current;
+           current = summ;
+       }
+       return current;
+    }
+}
